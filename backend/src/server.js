f65f6bf6eba72509dc8df79e3b92ce36365b620b
rashidoescode <rashi.bhojwani16@gmail.com>
2026-04-20 19:57:50 +0530

    Implemented signup using JWT

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import express from  'express';
 import dotenv from 'dotenv';
 import authRouters from "./routes/auth.route.js";
 import messageRouters from "./routes/message.auth.js";
+import { connectDB } from './lib/db.js';
 
 dotenv.config();
 
@@ -11,7 +12,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json())
+
 app.use("/api/auth" , authRouters);
 app.use("/api/messages" , messageRouters);
 
-app.listen(PORT , () => console.log("Server Running on port : "+ PORT));
+app.listen(PORT , () => {
+    console.log("Server Running on port : "+ PORT);
+    connectDB();
+});
