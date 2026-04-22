1b5b855ccfbecff3a4d6147df42c8bfec0bf4012
rashidoescode <rashi.bhojwani16@gmail.com>
2026-04-22 19:17:30 +0530

    Implemented login, logout, and a protected updateProfile route using middleware.

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,12 +5,14 @@ import authRouters from "./routes/auth.route.js";
 import messageRouters from "./routes/message.auth.js";
 import { connectDB } from './lib/db.js';
 import { ENV } from './lib/env.js';
+import cookieParser from "cookie-parser"
 
 const app = express();
 
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json())
+app.use(cookieParser());
 
 app.use("/api/auth" , authRouters);
 app.use("/api/messages" , messageRouters);
